8074023a77bc5815be7e4d9332f4b79c29edc884
Venu Poruri <vmporuri@gmail.com>
2024-06-07 19:06:14 -0500

    feat: add function to add games to db

--- src/db.py
+++ src/db.py
@@ -1,4 +1,5 @@
-from sqlmodel import create_engine
+from typing import Generator
+from sqlmodel import Session, create_engine
 from src import models
 
 sqlite_file_name = "./databases/user_data.db"
@@ -7,5 +8,19 @@ sqlite_url = f"sqlite:///{sqlite_file_name}"
 engine = create_engine(sqlite_url, echo=True)
 
 
-def add_games_to_db():
-    raise NotImplementedError()
+def add_games_to_db(
+    puzzles: Generator[models.Puzzle, None, None],
+    batch_size: int = 50,
+) -> None:
+    with Session(engine) as session:
+        batch = []
+        for puzzle in puzzles:
+            batch.append(puzzle)
+            if len(batch) >= batch_size:
+                session.add_all(batch)
+                session.commit()
+                batch.clear()
+
+        if batch:
+            session.add_all(batch)
+        session.commit()
